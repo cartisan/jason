ec42d5f8878a3a6d328b3a3124c233a533b35c57
cartisan <leon@die-genossen.de>
2020-02-03 16:16:57 +0100

    appraise_emotion now requires source and ensures consistent +source behavior

--- src/main/java/jason/asSemantics/AffectiveTransitionSystem.java
+++ src/main/java/jason/asSemantics/AffectiveTransitionSystem.java
@@ -15,7 +15,7 @@ import jason.asSyntax.parser.ParseException;
 import jason.runtime.Settings;
 
 public class AffectiveTransitionSystem extends TransitionSystem {
-    
+
     /**
      * Contains emotion names of emotions appraised during ASL reasoning.
      * Gets populated by {@link jason.stdlib.appraise_emotion}.
@@ -27,12 +27,12 @@ public class AffectiveTransitionSystem extends TransitionSystem {
         super(a, c, s, ar);
         this.init();
     }
-    
+
     AffectiveTransitionSystem(TransitionSystem ts) {
         super(ts.getAg(), ts.getC(), ts.getSettings(), ts.getUserAgArch());
         this.init();
     }
-    
+
     private void init() {
         AffectiveCircumstance affC = new AffectiveCircumstance(this.getC(), (AffectiveAgent) this.getAg());
         this.C = affC;
@@ -43,7 +43,7 @@ public class AffectiveTransitionSystem extends TransitionSystem {
     protected String startingStepDeliberate() {
         return "DeriveSEM";
     }
-    
+
     @Override
     public boolean canSleepDeliberate() {
         boolean canSleep = super.canSleepDeliberate() && this.getAffectiveC().getPEM().isEmpty();
@@ -52,7 +52,7 @@ public class AffectiveTransitionSystem extends TransitionSystem {
         }
         return canSleep;
     }
-    
+
     @Override
     protected void applySemanticRuleSense() throws JasonException {
         this.getLogger().fine(this.toString() + " sense step: " + this.stepSense);
@@ -70,35 +70,35 @@ public class AffectiveTransitionSystem extends TransitionSystem {
             case "DeriveSEM":     this.applyDeriveSEM(); break;  // TODO: change order to derive only 1 SEM after SelEv
             case "UpMood":        this.applyUpMood(); break;
         default:
-            super.applySemanticRuleDeliberate();  
+            super.applySemanticRuleDeliberate();
         }
     }
-    
+
     @Override
     protected void applySemanticRuleAct() throws JasonException {
         this.getLogger().fine(this.toString() + " act step: " + this.stepAct);
         super.applySemanticRuleAct();
     }
-    
+
     /* ------ Sense States ------------ */
     @Override
     protected void applyProcMsg() throws JasonException {
         super.applyProcMsg();
         this.stepSense = "DerivePEM";
     }
-    
+
     protected void applyDerivePEM() throws JasonException {
         this.stepSense = "SelEv";
-        
+
         // apply one step of decay to old pem
         this.getAffectiveC().stepDecayPEM();
-        
+
         // appraise new pem
         Iterator<Literal> perceptsIt = this.getAg().getBB().getPercepts();
-        
+
         while (perceptsIt.hasNext()) {
             Literal percept = perceptsIt.next();
-            
+
             // get all terms of form ´emotion(X)´/´emotion(X, Target)´ in annotations, or empty list if none present
             ListTerm emotions = percept.getAnnots(Emotion.ANNOTATION_FUNCTOR);
             for(Term emotionTerm: emotions) {
@@ -109,13 +109,13 @@ public class AffectiveTransitionSystem extends TransitionSystem {
                         throw new JasonException(emotionType + " is not a valid OCC emotion, check the catalogue in jason.asSemantics.Emotion");
                     }
                     Emotion emotion = Emotion.getEmotion(emotionType);
-                    
+
                     if(emotionLit.getArity() == 2) {
                         emotion.setTarget(emotionLit.getTerm(1).toString());
                     }
-                    
+
                     emotion.setCause(percept.toString());
-                    
+
                     this.getAffectiveAg().addEmotion(emotion, "PEM");
                 } catch (ParseException e) {
                     throw new JasonException(e.getMessage());
@@ -123,14 +123,14 @@ public class AffectiveTransitionSystem extends TransitionSystem {
             }
         }
     }
-    
+
     /* ------ Deliberate States ------------ */
     @Override
     protected void applySelEv() throws JasonException {
         // Rule for atomic, if there is an atomic intention, do not select event
         if (this.C.hasAtomicIntention()) {
             this.stepDeliberate = "ProcAct"; // need to go to ProcAct to see if an atomic intention received a feedback action
-            return;            
+            return;
         }
 
         // Rule for atomic, events from atomic intention have priority
@@ -144,7 +144,7 @@ public class AffectiveTransitionSystem extends TransitionSystem {
             // first deal with +/-affective events, so we don't end up deliberating based on wrong beliefs
             for(Event ev : this.C.getEvents()) {
                 if (ev.getTrigger().getPredicateIndicator().getFunctor().endsWith("mood") |
-                     ev.getTrigger().getPredicateIndicator().getFunctor().endsWith("affect_target") | 
+                     ev.getTrigger().getPredicateIndicator().getFunctor().endsWith("affect_target") |
                      ev.getTrigger().getPredicateIndicator().getFunctor().endsWith("emotion")) {
                     // TODO: refactor this into AffectiveAgent#selectEvent!
                     this.C.getEvents().remove(ev);
@@ -152,8 +152,8 @@ public class AffectiveTransitionSystem extends TransitionSystem {
                     this.stepDeliberate = "RelPl";
                     return;
                 }
-            }           
-            
+            }
+
             // Rule SelEv1 -- like original TransitionSystem.applySelEv
             this.C.SE = this.getAg().selectEvent(this.C.getEvents());
             if (this.getLogger().isLoggable(Level.FINE)) {
@@ -168,21 +168,21 @@ public class AffectiveTransitionSystem extends TransitionSystem {
         // directly to ProcAct if no event to handle
         this.stepDeliberate = "ProcAct";
     }
-    
+
     @Override
     protected void applyFindOp() throws JasonException {
         // can't use optimized AplPl selection cause SEM need access to C.RP which are not generated here
         // go back to original reasoning cycle
         this.stepDeliberate = "RelPl";
     }
-    
+
     protected void applyDeriveSEM() throws JasonException {
         this.stepDeliberate = "UpMood";
-        
+
         // apply one decay step on old sem
         this.getAffectiveC().stepDecaySEM();
-        
-        
+
+
         // derive new sem
         // TODO: find if currentEvent triggers a secondary emotion, using something like this.C.SE == emotion.cause
         synchronized(this.deliberative_appraisal) {
@@ -192,130 +192,127 @@ public class AffectiveTransitionSystem extends TransitionSystem {
             this.deliberative_appraisal.clear();
         }
     }
-    
+
     protected void applyUpMood() throws JasonException {
         this.stepDeliberate = "SelEv";
         Mood oldMood = this.getAffectiveC().getM().clone();
         List<Emotion> emotions = this.getAffectiveC().getAllEmotions();
-        
+
         if(emotions.isEmpty()) {
             // perform one step of decay on old mood
-            this.getAffectiveC().getM().stepDecay(this.getAffectiveAg().getDefaultMood(), 
+            this.getAffectiveC().getM().stepDecay(this.getAffectiveAg().getDefaultMood(),
                                                   this.getAffectiveAg().getPersonality());
         }
         else {
             // perform one step of mood update
             this.getAffectiveC().getM().updateMood(emotions, this.getAffectiveAg().getPersonality());
         }
-        
+
         Mood newMood = this.getAffectiveC().getM().clone();
-        
+
         // allow agent to react to change of mood values
         if(!oldMood.equals(newMood)) {
             this.getAffectiveAg().updateMoodValue(newMood);
-            
+
             // if mood changed octants, update agent beliefs and reset target list
             if(oldMood.getType() != newMood.getType()){
                 this.getAffectiveAg().updateMoodType(oldMood, newMood);
-            } 
+            }
         }
-        
+
         // see if some of the current emotions contributed directly to current mood, if yes extract targets and sources
         for (Emotion emotion: this.getAffectiveC().getAllEmotions()) {
             if (Affect.getOctant(emotion).equals(Affect.getOctant(newMood))) {
                 //emotion contributes to current mood
                 this.getAffectiveAg().addMoodSource(emotion.cause);
-                
+
                 if (emotion.hasTarget()) {
                     this.getAffectiveAg().addMoodTarget(emotion.target);
                 }
             }
         }
-        
+
         this.getAffectiveAg().updateMoodTarget();
     }
-    
+
     @Override
     protected void applyApplPl() throws JasonException {
         super.applyApplPl();
-        
+
         if(this.C.AP == null) {
             return;
         }
-        
+
         // there are applicable plans available, check all of them for personality annotations
         //   if annotations are present, they all have to fit this agent in order for the plan to be applicable, otherwise remove from AP
         //   if plans with fitting personality annotations are present, consider only these specialized options for AP
         List<Option> specialisedOptions = new LinkedList<>();
-        
+
         for (Iterator<Option> it = this.C.AP.iterator(); it.hasNext(); ) {
             Option o = it.next();
-            
+
             Literal affect_condition = (Literal) o.getPlan().getLabel().getAnnots(Affect.ANNOTATION_FUNCTOR).getTerm();
             if (affect_condition != null) {
-              List<Term> innerConditions = affect_condition.getTerms();               
+              List<Term> innerConditions = affect_condition.getTerms();
               if(!(innerConditions.size()==1)) {
                   // list of conditions instead of: functor(term1,term2)
                   // e.g. affect(personality(E,hi),mood(P,lo))
                   this.getLogger().severe("*** ERROR in AffectiveTransitionSystem::applyApplPl >> Plan annotation " +
-                                     affect_condition.toString() + 
+                                     affect_condition.toString() +
                                      " is malformed, too many terms.");
                   throw new RuntimeException(affect_condition.toString() + " not a valid affective constraint.");
               }
-                
+
                 // a valid inner affective condition is present in this option
                 // form e.g. :[affect(and(personality(openness, high),not(mood(...)), ...))]
                 if(this.getAffectiveAg().checkConstraint((Literal) innerConditions.get(0))) {
-                    // if all terms in the annotation fit our agent, save this option as a specialized option 
+                    // if all terms in the annotation fit our agent, save this option as a specialized option
                     specialisedOptions.add(o);
                 } else {
                     // at least one annotation doesn't fit this personality, can't use this option
                     it.remove();
+                }
             }
-            }
-            
+
         }
-        
+
         // specialized plans that have a personality annotation fitting to this agent have been found, prefer these plans
         if (!specialisedOptions.isEmpty()) {
             this.C.AP = specialisedOptions;
             return;
         }
-        
+
         //we've been removing options in AP, if no options are left, proceed as usual in such a case
         if (this.C.AP.isEmpty()) {
             this.applyRelApplPlRule2("applicable");
+        }
     }
 
-    public void scheduleForAppraisal(String emotion) throws JasonException {
-        this.scheduleForAppraisal(emotion, null, null);
+    public void scheduleForAppraisal(String emotion, String source) throws JasonException {
+        this.scheduleForAppraisal(emotion, source, null);
     }
-    
-    public void scheduleForAppraisal(String emotion, String target) throws JasonException {
-        this.scheduleForAppraisal(emotion, target, null);
-    }
-    
-    public void scheduleForAppraisal(String emotion, String target, String source) throws JasonException {
+
+    public void scheduleForAppraisal(String emotion, String source, String target) throws JasonException {
         if (!Emotion.getAllEmotions().contains(emotion)) {
             throw new JasonException(emotion + " is not a valid OCC emotion, check the catalogue in jason.asSemantics.Emotion");
         }
-        
+
         Emotion em = Emotion.getEmotion(emotion);
-        
-        if((null != target) & (!target.equals("\"\"")))
+        em.setCause(source);
+
+        if(null != target && !target.equals("\"\"")) {
             em.setTarget(target);
-        if(null != source)
-            em.setCause(source);
-        
+        }
+
         this.deliberative_appraisal.add(em);
     }
-    
+
     public AffectiveCircumstance getAffectiveC() {
         return (AffectiveCircumstance) this.getC();
     }
-    
+
     public AffectiveAgent getAffectiveAg() {
         return (AffectiveAgent) this.getAg();
     }
-    
+
 }
--- src/main/java/jason/stdlib/appraise_emotion.java
+++ src/main/java/jason/stdlib/appraise_emotion.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package jason.stdlib;
 
@@ -15,82 +15,80 @@ import jason.asSyntax.Term;
 
 /**
  *<p>Internal action: <b><code>.appraise_emotion</code></b>.
-  
-  <p>Description: chains the provided deliberative emotion for appraisal by the affective architecture during secondary 
+
+  <p>Description: chains the provided deliberative emotion for appraisal by the affective architecture during secondary
   emotion appraisal.  Can only be used when AffectiveAgent (sub)class is selected as agentClass.
-  
+
   <p>Parameters:<ul>
   <li>emotion   (atom): the emotion to be appraised, see {@link jason.asSemantics.Emotion} for emotion list
+  <li>source    (string): the event that caused the emotion, variables will be unified
   <li>target    (atom): the agent the emotion is targeted at [optional]
-  <li>source    (string): the event that caused the emotion, variables will be unified [optional]
-  <li>addition  (boolean): true if source was a belief addition, false if belief removal [optional]
+  <li>deletion  (boolean): true if source was a belief deletion, false if belief addition (default: false) [optional]
   </ul>
-  
-  Examples:<ul> 
+
+  Examples:<ul>
   <li> <code>.appraise_emotion(joy)</code>: adds "joy" to list of deliberative emotions to be appraised next cycle </li>
-  <li> <code>.appraise_emotion(angry, bob)</code>: adds an angry emotion targeted at the agent bob </li>
-  <li> <code>.appraise_emotion(angry, bob, "stole(bob,X)[source(self)]")</code>: adds an angry emotion targeted at the agent
-   			  bob with the cause annotation <code>stole(bob,cheese)</code> if X unifying to cheese. </li>
-  <li> <code>.appraise_emotion(angry, bob, "love(bob, self)", false)</code>: adds an angry emotion targeted at the agent
-   			  bob with the cause annotation <code>-love(bob,self)</code>. </li>
+  <li> <code>.appraise_emotion(angry, "stole(_,X)[source(self)]")</code>: adds an angry emotion with the cause annotation
+              <code>stole(_,cheese)</code> if X unifies to cheese. </li>
+  <li> <code>.appraise_emotion(angry, "stole(bob,X)[source(self)]", bob)</code>: adds an angry emotion targeted at the agent
+              bob with the cause annotation <code>stole(bob,cheese)</code> if X unifying to cheese. </li>
+  <li> <code>.appraise_emotion(angry, "love(bob, self)", bob, true)</code>: adds an angry emotion targeted at the agent
+              bob with the cause annotation <code>-love(bob,self)</code>. </li>
   </ul>
-  
+
   @author Leonid Berov
  */
 public class appraise_emotion extends DefaultInternalAction {
-    
+
     @Override
-    public int getMinArgs() { 
-        return 1; 
+    public int getMinArgs() {
+        return 2;
     }
 
     @Override
-    public int getMaxArgs() { 
+    public int getMaxArgs() {
         return 4;
     }
-    
+
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
-        checkArguments(args);
-        
+        this.checkArguments(args);
+
+        String emotion = args[0].toString();
+        String cause;
+        if (args[1].isString()) {
+            String source = ((StringTermImpl) args[1]).getString();
+            Term unifiedSource = ASSyntax.parseLiteral(source).capply(un);
+            cause = unifiedSource.toString();
+        } else {
+            throw new JasonException("2nd argument of internal action: appraise_emotion should be a string, was: " + args[1]);
+        }
+
         switch(args.length) {
-            case 1: ((AffectiveTransitionSystem) ts).scheduleForAppraisal(args[0].toString()); return true;
-            case 2: ((AffectiveTransitionSystem) ts).scheduleForAppraisal(args[0].toString(), args[1].toString()); return true;
+            case 2:{
+                // in standard case, assume emotion results from addition of a believe
+                ((AffectiveTransitionSystem) ts).scheduleForAppraisal(emotion, "+" + cause);
+                return true;
+            }
             case 3: {
-                if (args[2].isString()) {
-                    String source = ((StringTermImpl) args[2]).getString();
-                    Term unifiedSource = ASSyntax.parseLiteral(source).capply(un);
-                    
-                    // in standard case, assume emotion results from addition of a believe
-                    ((AffectiveTransitionSystem) ts).scheduleForAppraisal(args[0].toString(),
-                                                                          args[1].toString(),
-                                                                          unifiedSource.toString());  
-                    return true;
-                } else {
-                    throw new JasonException("3rd argument of internal action: appraise_emotion should be a string");
-                }
+                // in standard case, assume emotion results from addition of a believe
+                ((AffectiveTransitionSystem) ts).scheduleForAppraisal(emotion, "+" + cause, args[2].toString());
+                return true;
             }
             case 4: {
-                if (args[2].isString()) {
-                    String source = ((StringTermImpl) args[2]).getString();
-                    Term unifiedSource = ASSyntax.parseLiteral(source).capply(un);
-                    
-                    boolean isAddition = args[3].equals(Literal.LTrue);
-                    if(isAddition) {
-                        source = "+";
-                    } else 
-                        source = "-";
-                    source += unifiedSource.toString();
-                    ((AffectiveTransitionSystem) ts).scheduleForAppraisal(args[0].toString(),
-                                                                          args[1].toString(),
-                                                                          source);
+                if(args[3].equals(Literal.LTrue)) {
+                    ((AffectiveTransitionSystem) ts).scheduleForAppraisal(emotion, "-" + cause, args[2].toString());
                     return true;
-                } else {
-                    throw new JasonException("3rd argument of internal action: appraise_emotion should be a string");
                 }
+                if(args[3].equals(Literal.LFalse)) {
+                    ((AffectiveTransitionSystem) ts).scheduleForAppraisal(emotion, "+" + cause, args[2].toString());
+                    return true;
+                }
+
+                throw new JasonException("4th argument of internal action: appraise_emotion should be a boolean, was: " + args[3]);
             }
             default: throw new JasonException("Wrong number of arguments provided for internal action: appraise_emotion");
         }
     }
-    
+
 }
