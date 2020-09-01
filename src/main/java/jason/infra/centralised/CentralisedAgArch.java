6337002cc3155a7cdd92a7b69396d7aa865a34f7
cartisan <leon@die-genossen.de>
2020-09-01 19:31:04 +0200

    Enabling reasoning cycles to sleep in sync mode. BEWARE: EXPERIMENTAL!

--- src/main/java/jason/infra/centralised/CentralisedAgArch.java
+++ src/main/java/jason/infra/centralised/CentralisedAgArch.java
@@ -26,9 +26,9 @@ import jason.util.Config;
 /**
  * This class provides an agent architecture when using Centralised
  * infrastructure to run the MAS inside Jason.
- * 
+ *
  * Each agent has its own thread.
- * 
+ *
  * <p>
  * Execution sequence:
  * <ul>
@@ -47,13 +47,13 @@ public class CentralisedAgArch extends AgArch implements Runnable {
 
     private String           agName  = "";
     private volatile boolean running = true;
-    private Queue<Message>   mbox    = new ConcurrentLinkedQueue<Message>(); 
+    private Queue<Message>   mbox    = new ConcurrentLinkedQueue<>();
     protected Logger         logger  = Logger.getLogger(CentralisedAgArch.class.getName());
-    
+
     private static List<MsgListener> msgListeners = null;
     public static void addMsgListener(MsgListener l) {
         if (msgListeners == null) {
-            msgListeners = new ArrayList<MsgListener>();
+            msgListeners = new ArrayList<>();
         }
         msgListeners.add(l);
     }
@@ -73,19 +73,19 @@ public class CentralisedAgArch extends AgArch implements Runnable {
         try {
             this.masRunner = masRunner;
             Agent.create(this, agClass, bbPars, asSrc, stts);
-            insertAgArch(this);
-            
-            createCustomArchs(agArchClasses);
+            this.insertAgArch(this);
+
+            this.createCustomArchs(agArchClasses);
 
             // mind inspector arch
             if (stts.getUserParameter(Settings.MIND_INSPECTOR) != null) {
-                insertAgArch( (AgArch)Class.forName( Config.get().getMindInspectorArchClassName()).newInstance() );
-                getFirstAgArch().init();
+                this.insertAgArch( (AgArch)Class.forName( Config.get().getMindInspectorArchClassName()).newInstance() );
+                this.getFirstAgArch().init();
             }
-            
-            setLogger();
+
+            this.setLogger();
         } catch (Exception e) {
-            running = false;
+            this.running = false;
             throw new JasonException("as2j: error creating the agent class! - "+e.getMessage(), e);
         }
     }
@@ -94,276 +94,297 @@ public class CentralisedAgArch extends AgArch implements Runnable {
     public void createArchs(List<String> agArchClasses, Agent ag, BaseCentralisedMAS masRunner) throws JasonException {
         try {
             this.masRunner = masRunner;
-            setTS(ag.clone(this).getTS());
-            insertAgArch(this);
+            this.setTS(ag.clone(this).getTS());
+            this.insertAgArch(this);
 
-            createCustomArchs(agArchClasses);
+            this.createCustomArchs(agArchClasses);
 
-            setLogger();
+            this.setLogger();
         } catch (Exception e) {
-            running = false;
+            this.running = false;
             throw new JasonException("as2j: error creating the agent class! - ", e);
         }
     }
-    
-    
+
+
     public void stopAg() {
-        running = false;
-        wake(); // so that it leaves the run loop
-        if (myThread != null)  
-            myThread.interrupt();
-        getTS().getAg().stopAg();
-        getUserAgArch().stop(); // stops all archs
+        this.running = false;
+        this.wake(); // so that it leaves the run loop
+        if (this.myThread != null) {
+            this.myThread.interrupt();
+        }
+        this.getTS().getAg().stopAg();
+        this.getUserAgArch().stop(); // stops all archs
     }
-    
+
 
     public void setLogger() {
-        logger = Logger.getLogger(CentralisedAgArch.class.getName() + "." + getAgName());
-        if (getTS().getSettings().verbose() >= 0)
-            logger.setLevel(getTS().getSettings().logLevel());       
+        this.logger = Logger.getLogger(CentralisedAgArch.class.getName() + "." + this.getAgName());
+        if (this.getTS().getSettings().verbose() >= 0) {
+            this.logger.setLevel(this.getTS().getSettings().logLevel());
+        }
     }
-    
+
     public Logger getLogger() {
-        return logger;
+        return this.logger;
     }
 
     public void setAgName(String name) throws JasonException {
-        if (name.equals("self"))
+        if (name.equals("self")) {
             throw new JasonException("an agent cannot be named 'self'!");
-        if (name.equals("percept"))
+        }
+        if (name.equals("percept")) {
             throw new JasonException("an agent cannot be named 'percept'!");
-        agName = name;
+        }
+        this.agName = name;
     }
 
     public String getAgName() {
-        return agName;
+        return this.agName;
     }
-    
+
     public AgArch getUserAgArch() {
-        return getFirstAgArch();
+        return this.getFirstAgArch();
     }
 
     public void setEnvInfraTier(CentralisedEnvironment env) {
-        infraEnv = env;
+        this.infraEnv = env;
     }
 
     public CentralisedEnvironment getEnvInfraTier() {
-        return infraEnv;
+        return this.infraEnv;
     }
 
     public void setControlInfraTier(CentralisedExecutionControl pControl) {
-        infraControl = pControl;
+        this.infraControl = pControl;
     }
 
     public CentralisedExecutionControl getControlInfraTier() {
-        return infraControl;
+        return this.infraControl;
     }
 
     private Thread myThread = null;
-    public void setThread(Thread t) { 
-        myThread = t;
-        myThread.setName(agName);
+    public void setThread(Thread t) {
+        this.myThread = t;
+        this.myThread.setName(this.agName);
     }
 
     public void startThread() {
-        myThread.start();
+        this.myThread.start();
     }
-    
+
     public boolean isRunning() {
-        return running;
+        return this.running;
     }
-    
+
     protected void sense() {
-        TransitionSystem ts = getTS();
-        
+        TransitionSystem ts = this.getTS();
+
         int i = 0;
         do {
             ts.sense(); // must run at least once, so that perceive() is called
-        } while (running && ++i < cyclesSense && !ts.canSleepSense());
+        } while (this.running && ++i < this.cyclesSense && !ts.canSleepSense());
     }
-    
+
     //int sumDel = 0; int nbDel = 0;
     protected void deliberate() {
-        TransitionSystem ts = getTS();
+        TransitionSystem ts = this.getTS();
         int i = 0;
-        while (running && i++ < cyclesDeliberate && !ts.canSleepDeliberate()) {
+        while (this.running && i++ < this.cyclesDeliberate && !ts.canSleepDeliberate()) {
             ts.deliberate();
         }
         //sumDel += i; nbDel++;
         //System.out.println("running del "+(sumDel/nbDel)+"/"+cyclesDeliberate);
     }
-    
+
     //int sumAct = 0; int nbAct = 0;
     protected void act() {
-        TransitionSystem ts = getTS();
-        
+        TransitionSystem ts = this.getTS();
+
         int i = 0;
-        int ca = cyclesAct;
-        if (cyclesAct == 9999)
+        int ca = this.cyclesAct;
+        if (this.cyclesAct == 9999) {
             ca = ts.getC().getIntentions().size();
-        
-        while (running && i++ < ca && !ts.canSleepAct()) {
+        }
+
+        while (this.running && i++ < ca && !ts.canSleepAct()) {
             ts.act();
         }
         //sumAct += i; nbAct++;
         //System.out.println("running act "+(sumAct/nbAct)+"/"+ca);
     }
-    
+
     protected void reasoningCycle() {
-        sense();
-        deliberate();
-        act();
-    }    
-    
+        this.sense();
+        this.deliberate();
+        this.act();
+    }
+
     public void run() {
-        TransitionSystem ts = getTS();        
-        while (running) {
+        TransitionSystem ts = this.getTS();
+        while (this.running) {
             if (ts.getSettings().isSync()) {
-                waitSyncSignal();
-                reasoningCycle();
+                this.waitSyncSignal();
+                this.reasoningCycle();
+                if (ts.canSleep()) {
+                    this.sleep();
+                }
                 boolean isBreakPoint = false;
                 try {
                     isBreakPoint = ts.getC().getSelectedOption().getPlan().hasBreakpoint();
-                    if (logger.isLoggable(Level.FINE)) logger.fine("Informing controller that I finished a reasoning cycle "+getCycleNumber()+". Breakpoint is " + isBreakPoint);
+                    if (this.logger.isLoggable(Level.FINE)) {
+                        this.logger.fine("Informing controller that I finished a reasoning cycle "+this.getCycleNumber()+". Breakpoint is " + isBreakPoint);
+                    }
                 } catch (NullPointerException e) {
                     // no problem, there is no sel opt, no plan ....
                 }
-                informCycleFinished(isBreakPoint, getCycleNumber());
+                this.informCycleFinished(isBreakPoint, this.getCycleNumber());
             } else {
-                incCycleNumber();
-                reasoningCycle();
-                if (ts.canSleep())
-                    sleep();
+                this.incCycleNumber();
+                this.reasoningCycle();
+                if (ts.canSleep()) {
+                    this.sleep();
+                }
             }
         }
-        logger.fine("I finished!");
+        this.logger.fine("I finished!");
     }
 
     private Object sleepSync = new Object();
     private int    sleepTime = 50;
-    
+
     public static final int MAX_SLEEP = 1000;
-    
+
     public void sleep() {
         try {
-            if (!getTS().getSettings().isSync()) {
-                //logger.fine("Entering in sleep mode....");
-                synchronized (sleepSync) {
-                    sleepSync.wait(sleepTime); // wait for messages
-                    if (sleepTime < MAX_SLEEP)
-                        sleepTime += 100;
+            this.logger.fine("Entering in sleep mode....");
+            synchronized (this.sleepSync) {
+                this.sleepSync.wait(this.sleepTime); // wait for messages
+                if (this.sleepTime < MAX_SLEEP) {
+                    this.sleepTime += 100;
                 }
             }
         } catch (InterruptedException e) {
         } catch (Exception e) {
-            logger.log(Level.WARNING,"Error in sleep.", e);
+            this.logger.log(Level.WARNING,"Error in sleep.", e);
         }
     }
-    
+
     @Override
     public void wake() {
-        synchronized (sleepSync) {
-            sleepTime = 50;
-            sleepSync.notifyAll(); // notify sleep method
+        synchronized (this.sleepSync) {
+            this.sleepTime = 50;
+            this.sleepSync.notifyAll(); // notify sleep method
         }
     }
-    
+
     @Override
     public void wakeUpSense() {
-        wake();
+        this.wake();
     }
-    
+
     @Override
     public void wakeUpDeliberate() {
-        wake();
+        this.wake();
     }
-    
+
     @Override
     public void wakeUpAct() {
-        wake();
+        this.wake();
     }
 
     // Default perception assumes Complete and Accurate sensing.
     @Override
     public Collection<Literal> perceive() {
         super.perceive();
-        if (infraEnv == null) return null;
-        Collection<Literal> percepts = infraEnv.getUserEnvironment().getPercepts(getAgName());
-        if (logger.isLoggable(Level.FINE) && percepts != null) logger.fine("percepts: " + percepts);
+        if (this.infraEnv == null) {
+            return null;
+        }
+        Collection<Literal> percepts = this.infraEnv.getUserEnvironment().getPercepts(this.getAgName());
+        if (this.logger.isLoggable(Level.FINE) && percepts != null) {
+            this.logger.fine("percepts: " + percepts);
+        }
         return percepts;
     }
 
     // this is used by the .send internal action in stdlib
     public void sendMsg(Message m) throws ReceiverNotFoundException {
         // actually send the message
-        if (m.getSender() == null)  m.setSender(getAgName());
-        
-        CentralisedAgArch rec = masRunner.getAg(m.getReceiver());
-            
+        if (m.getSender() == null) {
+            m.setSender(this.getAgName());
+        }
+
+        CentralisedAgArch rec = this.masRunner.getAg(m.getReceiver());
+
         if (rec == null) {
-            if (isRunning())
+            if (this.isRunning()) {
                 throw new ReceiverNotFoundException("Receiver '" + m.getReceiver() + "' does not exist! Could not send " + m);
-            else
+            } else {
                 return;
+            }
         }
         rec.receiveMsg(m.clone()); // send a cloned message
-    
+
         // notify listeners
-        if (msgListeners != null) 
-            for (MsgListener l: msgListeners) 
+        if (msgListeners != null) {
+            for (MsgListener l: msgListeners) {
                 l.msgSent(m);
+            }
+        }
     }
-    
+
     public void receiveMsg(Message m) {
-        mbox.offer(m);
-        wakeUpSense();
+        this.mbox.offer(m);
+        this.wakeUpSense();
     }
 
     public void broadcast(jason.asSemantics.Message m) throws Exception {
-        for (String agName: masRunner.getAgs().keySet()) {
+        for (String agName: this.masRunner.getAgs().keySet()) {
             if (!agName.equals(this.getAgName())) {
                 m.setReceiver(agName);
-                sendMsg(m);
+                this.sendMsg(m);
             }
         }
     }
 
     // Default procedure for checking messages, move message from local mbox to C.mbox
     public void checkMail() {
-        Circumstance C = getTS().getC();
-        Message im = mbox.poll();
+        Circumstance C = this.getTS().getC();
+        Message im = this.mbox.poll();
         while (im != null) {
             C.addMsg(im);
-            if (logger.isLoggable(Level.FINE)) logger.fine("received message: " + im);
-            im = mbox.poll();
+            if (this.logger.isLoggable(Level.FINE)) {
+                this.logger.fine("received message: " + im);
+            }
+            im = this.mbox.poll();
         }
     }
-    
+
     public Collection<Message> getMBox() {
-        return mbox;
+        return this.mbox;
     }
 
     /** called by the TS to ask the execution of an action in the environment */
     @Override
     public void act(ActionExec action) {
         //if (logger.isLoggable(Level.FINE)) logger.fine("doing: " + action.getActionTerm());
-        
-        if (isRunning()) { 
-            if (infraEnv != null) {
-                infraEnv.act(getAgName(), action);
+
+        if (this.isRunning()) {
+            if (this.infraEnv != null) {
+                this.infraEnv.act(this.getAgName(), action);
             } else {
                 action.setResult(false);
                 action.setFailureReason(new Atom("noenv"), "no environment configured!");
-                actionExecuted(action);                
+                this.actionExecuted(action);
             }
         }
     }
-    
+
     public boolean canSleep() {
-        return mbox.isEmpty() && isRunning();
+        return this.mbox.isEmpty() && this.isRunning();
     }
 
-    private Object  syncMonitor                = new Object(); 
+    private Object  syncMonitor                = new Object();
     private volatile boolean inWaitSyncMonitor = false;
 
     /**
@@ -372,14 +393,14 @@ public class CentralisedAgArch extends AgArch implements Runnable {
      */
     private void waitSyncSignal() {
         try {
-            synchronized (syncMonitor) {
-                inWaitSyncMonitor = true;
-                syncMonitor.wait();
-                inWaitSyncMonitor = false;
+            synchronized (this.syncMonitor) {
+                this.inWaitSyncMonitor = true;
+                this.syncMonitor.wait();
+                this.inWaitSyncMonitor = false;
             }
         } catch (InterruptedException e) {
         } catch (Exception e) {
-            logger.log(Level.WARNING,"Error waiting sync (1)", e);
+            this.logger.log(Level.WARNING,"Error waiting sync (1)", e);
         }
     }
 
@@ -389,34 +410,34 @@ public class CentralisedAgArch extends AgArch implements Runnable {
      */
     public void receiveSyncSignal() {
         try {
-            synchronized (syncMonitor) {
-                while (!inWaitSyncMonitor && isRunning()) {
+            synchronized (this.syncMonitor) {
+                while (!this.inWaitSyncMonitor && this.isRunning()) {
                     // waits the agent to enter in waitSyncSignal
-                    syncMonitor.wait(50); 
+                    this.syncMonitor.wait(50);
                 }
-                syncMonitor.notifyAll();
+                this.syncMonitor.notifyAll();
             }
         } catch (InterruptedException e) {
         } catch (Exception e) {
-            logger.log(Level.WARNING,"Error waiting sync (2)", e);
+            this.logger.log(Level.WARNING,"Error waiting sync (2)", e);
         }
     }
 
-    /** 
-     *  Informs the infrastructure tier controller that the agent 
+    /**
+     *  Informs the infrastructure tier controller that the agent
      *  has finished its reasoning cycle (used in sync mode).
-     *  
-     *  <p><i>breakpoint</i> is true in case the agent selected one plan 
-     *  with the "breakpoint" annotation.  
-     */ 
+     *
+     *  <p><i>breakpoint</i> is true in case the agent selected one plan
+     *  with the "breakpoint" annotation.
+     */
     public void informCycleFinished(boolean breakpoint, int cycle) {
-        infraControl.receiveFinishedCycle(getAgName(), breakpoint, cycle);
+        this.infraControl.receiveFinishedCycle(this.getAgName(), breakpoint, cycle);
     }
 
     public RuntimeServicesInfraTier getRuntimeServices() {
-        return new CentralisedRuntimeServices(masRunner);
+        return new CentralisedRuntimeServices(this.masRunner);
     }
-    
+
     private RConf conf;
 
     private int cycles = 1;
@@ -430,11 +451,11 @@ public class CentralisedAgArch extends AgArch implements Runnable {
     }
 
     public RConf getConf() {
-        return conf;
+        return this.conf;
     }
 
     public int getCycles() {
-        return cycles;
+        return this.cycles;
     }
 
     public void setCycles(int cycles) {
@@ -442,7 +463,7 @@ public class CentralisedAgArch extends AgArch implements Runnable {
     }
 
     public int getCyclesSense() {
-        return cyclesSense;
+        return this.cyclesSense;
     }
 
     public void setCyclesSense(int cyclesSense) {
@@ -450,7 +471,7 @@ public class CentralisedAgArch extends AgArch implements Runnable {
     }
 
     public int getCyclesDeliberate() {
-        return cyclesDeliberate;
+        return this.cyclesDeliberate;
     }
 
     public void setCyclesDeliberate(int cyclesDeliberate) {
@@ -458,7 +479,7 @@ public class CentralisedAgArch extends AgArch implements Runnable {
     }
 
     public int getCyclesAct() {
-        return cyclesAct;
+        return this.cyclesAct;
     }
 
     public void setCyclesAct(int cyclesAct) {
